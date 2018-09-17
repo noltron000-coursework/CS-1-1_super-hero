d424494020d6f3eee987fcf9c8b94359e3538b33
Nolan Kovacik <noltron000@gmail.com>
2018-09-17 12:22:24 -0700

    dogs are done :)

--- doggo.py
+++ doggo.py
@@ -1,10 +1,26 @@
 class Dog:
-	def bark(self):
-		print("Woof!")
+	greeting = "Woof!"
 
-my_dog = Dog()
-my_dog.bark()
+	def __init__(self, name):
+		self.name = name
+
+	def bark(self):
+		print(self.greeting)
+
+
 
 if __name__ == "__main__":
-	my_dog = Dog()
+	my_dog = Dog("Spot")
+	my_other_dog = Dog("Stacy")
 	my_dog.bark()
+	print(my_dog.name)
+	print(my_other_dog.name)
+
+my_first_dog = Dog("Annie")
+my_second_dog = Dog("Wyatt")
+
+print(my_first_dog.name)
+print(my_second_dog.name)
+
+my_first_dog.bark()
+my_second_dog.bark()
